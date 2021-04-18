846c53e11888ff1319b29ae453c6bcd66a5201db
Bogdan <bogdankuzzmin@gmail.com>
2021-04-18 14:44:22 +0600

    added WrapperLayout hoc

---
+++ src/hocs/WrapperLayout/WrapperLayout.js
@@ -0,0 +1,11 @@
+import classes from './WrapperLayout.module.scss';
+
+const wrapperLayout = props => {
+  return (
+    <div className={classes.WrapperLayout}>
+      {props.children}
+    </div>
+  );
+};
+
+export default wrapperLayout;
