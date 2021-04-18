5f86830de9f19ccc34b174410b8f24fa9b603903
Bogdan <bogdankuzzmin@gmail.com>
2021-04-18 14:48:11 +0600

    added constants for Navigation

---
+++ src/constants/navigationList.js
@@ -0,0 +1,16 @@
+const NavigationLinks = {
+  HOME: {
+    link: 'home',
+    name: 'Главная',
+  },
+  RENT: {
+    link: 'rent',
+    name: 'Аренда',
+  },
+  CONTACTS: {
+    link: 'contacts',
+    name: 'Контакты',
+  },
+};
+
+export default NavigationLinks;
