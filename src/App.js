6e69fd072abbd60675267df64511191f58e2efed
Bogdan <bogdankuzzmin@gmail.com>
2021-04-18 14:50:53 +0600

    [App] - added Layout, Rent, MainPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import {Route, Switch} from 'react-router-dom';
+
+import NavigationLinks from './constants/navigationList';
+
+import Layout from './hocs/Layout';
+import MainPage from './containers/MainPage';
+import Rent from './containers/Rent';
+
 const App = props => {
   return (
-    <p>Let's get started</p>
+    <Layout>
+      <Switch>
+        <Route path={`/${NavigationLinks.HOME.link}`} component={MainPage} />
+        <Route path={`/${NavigationLinks.RENT.link}`} component={Rent} />
+        
+        <Route render={() => <p>Page was not found!</p>} />
+      </Switch>
+    </Layout>
   );
 };
 
