d3f38a3ab6597b7936c0d5bafb48c0af8b966d23
Bogdan <bogdankuzzmin@gmail.com>
2021-04-18 21:10:43 +0600

    [Rent] - added styles

--- src/containers/Rent/Rent.js
+++ src/containers/Rent/Rent.js
@@ -1,11 +1,40 @@
-import WrapperLayout from '../../hocs/WrapperLayout';
+import React, {useEffect, useCallback} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
-const rent = props => {
+import classes from './Rent.module.scss';
+
+import {fetchApartments} from '../../store/actions/apartment';
+
+import WrapperLayout from '../../hocs/WrapperLayout';
+import Spinner from '../../components/UI/Spinner';
+import ApartmentList from '../../components/ApartmentList';
+
+const Rent = props => {
+  const dispatch = useDispatch();
+
+  const apartments = useSelector(state => state.apartments);
+  const loading = useSelector(state => state.loading);
+
+  const fetchApartmentsHandler = useCallback(() => dispatch(fetchApartments()), [dispatch]);
+
+  useEffect(() => {
+    if (apartments.length === 0) {
+      fetchApartmentsHandler();
+    }
+  }, [fetchApartmentsHandler]);
+  
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
-    <WrapperLayout>
-      <p>Rent Page</p>
-    </WrapperLayout>
+    <section className={classes.Rent}>
+      <WrapperLayout>
+        <h2>Аренда жилья</h2>
+        <ApartmentList apartments={apartments} />
+      </WrapperLayout>
+    </section>
   );
 };
 
-export default rent;
+export default Rent;
