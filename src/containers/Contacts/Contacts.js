5ba2aa9bf05fa15bb028c78c43ddd3d4d5ea8df4
Bogdan <bogdankuzzmin@gmail.com>
2021-04-18 20:08:44 +0600

    added Contacts container component

---
+++ src/containers/Contacts/Contacts.js
@@ -0,0 +1,13 @@
+import WrapperLayout from '../../hocs/WrapperLayout';
+
+const contacts = props => {
+  return (
+    <section>
+      <WrapperLayout>
+        <h2>Контакты</h2>
+      </WrapperLayout>
+    </section>
+  );
+};
+
+export default contacts;
