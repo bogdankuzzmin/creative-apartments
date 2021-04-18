8f3e77c05accf472cc075a062a9b5c5b3705084c
Bogdan <bogdankuzzmin@gmail.com>
2021-04-18 17:18:58 +0600

    added Spinner

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {ReactComponent as SpinnerSvg} from '../../../assets/spinner.svg';
+
+const spinner = () => {
+  return (
+    <SpinnerSvg width="220" height="220" />
+  );
+};
+
+export default spinner;
