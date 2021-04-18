ac33d1aa07a572c694971ae7eaefbc6843d0aa66
Bogdan <bogdankuzzmin@gmail.com>
2021-04-18 17:15:53 +0600

    [index.js] - added router, redux, redux-devtools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 
-import './index.css';
+import './index.scss';
+
+import apartmentStore from './store/reducers/apartment';
 
 import App from './App';
 
+const composeEnhancers = process.env.NODE_ENV === 'development' ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const store = createStore(apartmentStore, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
